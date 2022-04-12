95619040ef507d4d5af2d7aa57e9a7a602e41cd1
Timur Khrustalyov <timur.khrustalyov@bstd.ru>
2022-04-12 21:42:38 +0300

    Husky

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      News App
-    </div>
-  );
+  return <div>News App</div>;
 }
 
 export default App;
