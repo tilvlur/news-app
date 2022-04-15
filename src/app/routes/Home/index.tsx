44c4fb01684265d11681b4b467df1d7b5c02d1fc
Timur Khrustalyov <timur.khrustalyov@bstd.ru>
2022-04-15 11:28:31 +0300

    Add Home page

--- src/app/routes/Home/index.tsx
+++ src/app/routes/Home/index.tsx
@@ -1,7 +1,16 @@
+import { useAppSelector } from "../../../common/hooks/hooks";
+import { selectLoginData } from "../../../features/login/loginSlice";
+import styles from "./Home.module.scss";
+
 function Home() {
+  const { currentUserData } = useAppSelector(selectLoginData);
+  const user = currentUserData ? currentUserData.userName : "гость";
+
   return (
-    <div>
-      <div>HOME</div>
+    <div className={styles.container}>
+      <div>
+        Привет, <span>{user}!</span>
+      </div>
     </div>
   );
 }
