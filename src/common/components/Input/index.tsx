fe541a9d1e3e2b18f4cab7d155fb1aab223ba54e
Timur Khrustalyov <timur.khrustalyov@bstd.ru>
2022-04-15 20:23:31 +0300

    Input mod

--- src/common/components/Input/index.tsx
+++ src/common/components/Input/index.tsx
@@ -2,15 +2,25 @@ import { ChangeEvent, memo } from "react";
 import styles from "./Input.module.scss";
 
 interface InputProps {
-  htmlFor: string;
-  label: string;
+  htmlFor?: string;
+  label?: string;
   type: "text" | "number" | "password";
   value: string;
+  placeholder?: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  isLabel?: boolean;
 }
 
-function Input({ htmlFor, label, type, value, onChange }: InputProps) {
-  return (
+function Input({
+  htmlFor,
+  label,
+  type,
+  value,
+  placeholder,
+  onChange,
+  isLabel = true,
+}: InputProps) {
+  return isLabel ? (
     <label htmlFor={htmlFor}>
       <div className={styles.label}>{label}</div>
       <input
@@ -18,9 +28,18 @@ function Input({ htmlFor, label, type, value, onChange }: InputProps) {
         className={styles.input}
         type={type}
         value={value}
+        placeholder={placeholder}
         onChange={onChange}
       />
     </label>
+  ) : (
+    <input
+      className={styles.input}
+      type={type}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
   );
 }
 
